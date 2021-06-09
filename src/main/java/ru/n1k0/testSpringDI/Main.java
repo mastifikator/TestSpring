104f08b74edeb470d132c070c0f8e7b886d611ba
mastifikator <mastifikator@yandex.ru>
2021-06-10 01:19:45 +0300

    Initial commit

---
+++ src/main/java/ru/n1k0/testSpringDI/Main.java
@@ -0,0 +1,19 @@
+package ru.n1k0.testSpringDI;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Battle battle1 = context.getBean("battle", Battle.class);
+        Battle battle2 = context.getBean("battle", Battle.class);
+        battle2.setName("Дуротар");
+
+        battle1.startBattle();
+        System.out.println();
+        battle2.startBattle();
+
+        context.close();
+    }
+}
---
+++ src/main/java/ru/n1k0/testSpringDI/Undead.java
@@ -0,0 +1,52 @@
+package ru.n1k0.testSpringDI;
+
+public class Undead extends Unit {
+    public Undead(String name, double strength, double speed) {
+        super(name, strength, speed);
+    }
+
+    @Override
+    public void doBattleRoar() {
+        System.out.println("За Нерзула!");
+    }
+
+    @Override
+    public void doVictoryRoar() {
+        System.out.println("Хозяин будет доволен!");
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public double getStrength() {
+        return super.getStrength();
+    }
+
+    @Override
+    public void setStrength(double strength) {
+        super.setStrength(strength);
+    }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        super.setSpeed(speed);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
